290a1bd3ddb8fed001a377c40975bbb6c598aa35
Maninder Singh <manindersingh030@gmail.com>
2017-04-07 21:27:55 +0530

    ready

--- tests/test1.php
+++ tests/test1.php
@@ -5,6 +5,5 @@ use OLDumpReader\OlReader;
 $dumpLocation =__DIR__.'/../../books/openlibrary/ol_dump_latest.txt.gz';
 $reader = new OLReader($dumpLocation); 
 
-
 $reader->seekToLastLocation();
 var_dump($reader->getNextPacket());
